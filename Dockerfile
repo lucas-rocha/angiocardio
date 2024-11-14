5954335dd674e3d56dcdc9d9819c3ff9da30520d
Lucas Rocha <lucasrocha.dv@gmail.com>
2024-11-14 18:10:34 -0300

    Update project

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+# Use a imagem base do Node.js
+FROM node:18
+
+# Defina o diretório de trabalho
+WORKDIR /app
+
+# Copie os arquivos de configuração do pacote
+COPY package.json package-lock.json ./
+
+# Instale as dependências
+RUN npm install
+
+# Copie o restante dos arquivos da aplicação
+COPY . .
+
+# Gere os tipos do Prisma
+RUN npx prisma generate
+
+# Exponha a porta 3000
+EXPOSE 3000
+
+# Comando para rodar a aplicação
+CMD ["npm", "run", "dev"]
--- src/app/(public-routes)/page.tsx
+++ src/app/(public-routes)/page.tsx
@@ -28,12 +28,8 @@ export default function Home() {
   return (
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            alt="Your Company"
-            src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
-            className="mx-auto h-10 w-auto"
-          />
+        <div className="sm:mx-auto sm:w-full sm:max-w-md">
+          <h1 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">Angiocardio Litoral Intervensionista</h1>
           <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-gray-900">
             Entre na sua conta
           </h2>
