7dd640dfc3f46ac04e7505d1bfa78c98120582f5
Lucas Rocha <lucasrocha.dv@gmail.com>
2024-12-10 11:21:08 -0300

    Connection string render

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverRuntimeConfig: {
+    port: process.env.PORT || 3000,
+  },
 };
 
 export default nextConfig;
