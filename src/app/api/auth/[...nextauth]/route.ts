6103059ab52e006a7a846b88aa6a4d3e83938a19
Lucas Rocha <lucasrocha.dv@gmail.com>
2024-12-10 11:59:30 -0300

    update nextauth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,54 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-expressions */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import NextAuth, { NextAuthOptions } from "next-auth"
-import CredentialsProvider from "next-auth/providers/credentials"
-
-const nextAuthOptions: NextAuthOptions = {
-  providers: [
-    CredentialsProvider({
-      name: 'credentials',
-      credentials: {
-        email: { label: 'email', type: 'text' },
-        password: {label: 'password', type: 'password'}
-      },
-
-      async authorize(credentials) {
-        const response = await fetch(`${process.env.NEXTAUTH_URL}/api/login`, {
-          method: 'POST',
-          headers: {
-            'Content-type': 'application/json'
-          },
-          body: JSON.stringify({
-            email: credentials?.email,
-            password: credentials?.password
-          })
-        })
-
-        const user = await response.json()
-
-        if(user && response.ok) {
-          return user
-        }
-        
-        return null
-        
-      }
-    })
-  ],
-  pages: {
-    signIn: '/'
-  },
-  callbacks: {
-    async jwt({ token, user }) {
-      user && (token.user = user)
-      return token
-    },
-    async session({ session, token }) {
-      session = token.user as any
-      return session
-    }
-  }
-}
+import nextAuthOptions from "@/utils/authOptions"
+import NextAuth from "next-auth"
 
 const handler = NextAuth(nextAuthOptions)
 
